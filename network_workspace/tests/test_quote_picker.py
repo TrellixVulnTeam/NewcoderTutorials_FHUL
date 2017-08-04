4e694ebc4fd09244404159c726232e010454aa1c
Randol Chance <randol@testnet.0.2.15>
2017-08-04 10:36:10 -0400

    Network WIP

---
+++ network_workspace/tests/test_quote_picker.py
@@ -0,0 +1,28 @@
+import os
+
+from twisted.trial import unittest
+
+from talkback.quote_picker import QuotePicker
+
+
+class TestQuotePicker(unittest.TestCase):
+    QUOTE1 = (
+	"A fool without fear is sometimes wiser than an angel with fear. "
+	"-Nancy Astor"
+    )
+    QUOTE2 = (
+	"You don't manage people, you manage things. You lead people."
+	"-Grace Hopper"
+    )
+    QUOTE3 = (
+	". . ."
+	"-Amy"
+    )
+
+    def test_pick(self):
+	picker = QuotePicker(
+	    os.path.join(os.path.dirname(__file__), "test_quotes.txt")
+	)
+	quote = picker.pick()
+	self.assertIn(quote, (self.QUOTE1, self.QUOTE2, self.QUOTE3),
+		      "Got unexpected quote: '%s'" % (quote))
---
+++ network_workspace/tests/test_talkbackbot.py
@@ -0,0 +1,4 @@
+from twisted.test import proto_helpers
+from twisted.test import unittest
+
+from talkback.bot import TalkBackBotFactory
